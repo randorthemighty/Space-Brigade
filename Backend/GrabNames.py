1d30942c12c5bbabe2791669ddf245121dd83442
Randy <tauyanrt6@gmail.com>
2025-10-05 14:28:23 -0700

    changed sep value

--- Backend/GrabNames.py
+++ Backend/GrabNames.py
@@ -7,7 +7,7 @@ import pprint as pp
 
 ## What We Want to Do is use this file without having to use local files
 fp = file_path = "Datasets(Temp)/sbdb_query_results(2).csv"
-df = pd.read_csv(fp, sep=';')
+df = pd.read_csv(fp)
 # Showing the file columns
 print(df.head(9))
 NamesId = [(name,id) for name in df['full_name'] for id in df['spkid']]
